8bf102df0abeee2a478e6da28cb031b00ea99618
Kamila Figueredo <kfigqa@gmail.com>
2022-01-20 15:13:23 -0300

    Adjusting 'home.spec' file to 'cypress.json' file defaults

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,14 +1,7 @@
 
-//decribe é o comando onde se cria o nome do caso de teste
-//()=> é uma função JS
-//it define o caso de teste
-//cy.visit acessa a api do site
-//cy.viewport controla o tamanho do navegador
-
 describe('home page', ()=>{
     it('App should be online', ()=>{
-        cy.viewport(1440, 900)
-        cy.visit('https://buger-eats.vercel.app')
+        cy.visit('/')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
     })
 })
